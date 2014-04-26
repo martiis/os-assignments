50bbb497513b1a735eee234c5cb66ab635a65589
martass <martynas12@gmail.com>
2014-04-27 01:02:07 +0300

    3.2:pathconf.c

--- LD3.2/loginas_pathconf.c
+++ LD3.2/loginas_pathconf.c
@@ -1,3 +1,34 @@
 /* Vardenis Pavardenis KTK varpav */
 /* Failas: loginas_pathconf.c */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int filenameLength()
+{
+    int result;
+    if ( (result = pathconf("/", _PC_NAME_MAX)) != -1 ) {
+        printf("_PC_NAME_MAX is %d\n", result);
+        return result;
+    } else {
+        return 0;
+    }
+}
+
+int pathLength()
+{
+    int result;
+    if( (result = pathconf("/", _PC_PATH_MAX)) != -1 ) {
+        printf("_PC_PATH_MAX is %d\n", result);
+        return result;
+    } else {
+        return 0;
+    }
+}
+
+main()
+{
+  filenameLength();
+  pathLength();
+}
