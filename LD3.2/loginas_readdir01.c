01e5e5867e9302ead489515f169412bb4f22e21a
martass <martynas12@gmail.com>
2014-04-27 12:40:51 +0300

    3.2:readdir01.c

--- LD3.2/loginas_readdir01.c
+++ LD3.2/loginas_readdir01.c
@@ -1,3 +1,35 @@
 /* Vardenis Pavardenis KTK varpav */
 /* Failas: loginas_readdir01.c */
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
+int openDirectory();
+
+int openDirectory()
+{
+    char *cwd;
+    struct dirent *dp;
+    cwd = getcwd( NULL, pathconf( ".", _PC_PATH_MAX) );
+    DIR *dir;
+    dir = opendir(cwd);
+
+    do {
+        if((dp = readdir(dir)) != NULL) {
+            //KAIP d_ino atspausdinti?!?
+            //puts(dp->d_ino);
+            printf("%s\n", dp->d_name);
+        }
+    } while(dp != NULL);
+
+    closedir(dir);
+}
+
+int main()
+{
+    openDirectory();
+    return 0;
+}
