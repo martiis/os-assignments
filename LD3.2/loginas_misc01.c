c503972408ea957d9441e2638d54f46ccb0ce0b7
martass <martynas12@gmail.com>
2014-04-27 20:30:30 +0300

    3/2:misc01.c+ntfw02.c

--- LD3.2/loginas_misc01.c
+++ LD3.2/loginas_misc01.c
@@ -1,3 +1,14 @@
 /* Vardenis Pavardenis KTK varpav */
 /* Failas: loginas_misc01.c */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    mkdir("./folderis", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    puts("Buvo sukurta nauja direktorija.");
+    return 0;
+}
--- LD3.2/loginas_nftw02.c
+++ LD3.2/loginas_nftw02.c
@@ -1,3 +1,31 @@
 /* Vardenis Pavardenis KTK varpav */
 /* Failas: loginas_nftw02.c */
 
+#define _XOPEN_SOURCE 500
+#include <ftw.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
+{
+    printf("%-3s %2d %7jd   %-40s %d %s\n",
+        (tflag == FTW_D) ?   "d"   : (tflag == FTW_DNR) ? "dnr" :
+        (tflag == FTW_DP) ?  "dp"  : (tflag == FTW_F) ?   "f" :
+        (tflag == FTW_NS) ?  "ns"  : (tflag == FTW_SL) ?  "sl" :
+        (tflag == FTW_SLN) ? "sln" : "???", ftwbuf->level, (intmax_t) sb->st_size, fpath, ftwbuf->base, fpath + ftwbuf->base);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+   int flags = 0;
+   flags |= FTW_PHYS;
+
+   if (nftw(getenv("HOME"), display_info, 20, flags) == -1) {
+        perror("nftw");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
