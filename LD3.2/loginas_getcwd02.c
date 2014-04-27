dfa850b6e797c20fc0a35208e07e3f7f7219335e
martass <martynas12@gmail.com>
2014-04-27 03:22:45 +0300

    3.2:getcwd02

--- LD3.2/loginas_getcwd02.c
+++ LD3.2/loginas_getcwd02.c
@@ -1,3 +1,35 @@
 /* Vardenis Pavardenis KTK varpav */
 /* Failas: loginas_getcwd02.c */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int getCwdTest();
+
+int getCwdTest()
+{
+    //1
+   char *cwd;
+   cwd = getcwd( NULL, pathconf( ".", _PC_PATH_MAX) );
+   puts(cwd);
+   //2
+   int dskr = open(cwd, O_RDONLY);
+   printf("Deskriptorius: %d\n", dskr);
+   //3
+   chdir("/tmp");
+   cwd = getcwd( NULL, pathconf( ".", _PC_PATH_MAX) );
+   puts(cwd);
+   //4
+   fchdir(dskr);
+   cwd = getcwd( NULL, pathconf( ".", _PC_PATH_MAX) );
+   puts(cwd);
+   return 1;
+}
+
+int main()
+{
+   getCwdTest();
+   return 0;
+}
