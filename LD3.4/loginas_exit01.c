2ee38c10369d953d1568a68feda6980e2d711045
martass <martynas12@gmail.com>
2014-04-29 15:03:06 +0300

    3/4:exit01

--- LD3.4/loginas_exit01.c
+++ LD3.4/loginas_exit01.c
@@ -1,3 +1,32 @@
 /* Vardenis Pavardenis KTU loginas */
 /* Failas: loginas_exit01.c */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void Funkcija1()
+{
+	puts("This is function one.");
+}
+
+void Funkcija2()
+{
+	puts("This is function two.");
+	exit(1);
+	puts("After exit");
+}
+void Funkcija3()
+{
+	puts("This is function three");
+	//abort();
+	//puts("after abort");
+}
+
+int main(int argc, char *argv[])
+{
+	atexit(Funkcija1);
+	atexit(Funkcija2);
+	atexit(Funkcija3);
+	puts("This is main function");
+	return 0;
+}
