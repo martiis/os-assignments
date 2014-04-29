2bc3b7f136b638ddd4c488c4eed331ae55e2a4ec
martass <martynas12@gmail.com>
2014-04-29 15:20:10 +0300

    3/4:clock

--- LD3.4/loginas_clock02.c
+++ LD3.4/loginas_clock02.c
@@ -1,3 +1,30 @@
 /* Vardenis Pavardenis KTU loginas */
 /* Failas: loginas_clock02.c */
+#include <stdio.h>
+#include <time.h>
 
+void getUTC()
+{
+time_t rawtime;
+  struct tm * ptm;
+  time ( &rawtime );
+  ptm = gmtime ( &rawtime );
+  printf ("UTC Time\t %2d:%02d\n", (ptm->tm_hour)%24, ptm->tm_min);
+}
+
+void getLocal()
+{
+        time_t rawtime;
+        struct tm * timeinfo;
+
+        time (&rawtime);
+        timeinfo = localtime (&rawtime);
+        printf ("Local Time\t%s", asctime(timeinfo));
+}
+
+int main()
+{
+	getLocal();
+	getUTC();
+  	return 0;
+}
