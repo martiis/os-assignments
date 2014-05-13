704e2180f8430853b61a2b680112e105bf58d06b
martass <martynas12@gmail.com>
2014-05-13 15:27:57 +0300

    init LD4

---
+++ LD4.1/loginas_fork02.c
@@ -0,0 +1,12 @@
+/* Vardenis Pavardenis KTU varpav */
+/* Failas: loginas_fork01.c */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+
+}
