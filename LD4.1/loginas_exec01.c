dac902d14384cc49ce3d11f7a6533ee6c2d508f7
martass <martynas12@gmail.com>
2014-05-20 21:18:29 +0300

    ld4.1/exec01.c

--- LD4.1/loginas_exec01.c
+++ LD4.1/loginas_exec01.c
@@ -12,14 +12,20 @@ int main(int argc, char * argv[])
 	if(argc == 2) {
 		int arg = atoi(argv[1]);
 		pid_t pid = getpid();
-      	 	pid_t ppid = getppid();
-		printf("PID: %ld\t PPID: %ld\t ARG: %d\n", pid, ppid, arg);
+      	pid_t ppid = getppid();
+		printf("PID: %d\t PPID: %d\t ARG: %d\n", pid, ppid, arg);
 			
 		if(arg > 0)
 		{
 			--arg;
-			char *const params[] = {(char*)arg};
-			execv(".", params);
+			char msg[3];
+			sprintf(msg, "%d", arg);
+			execl(
+			     "/home/martynas/Projects/os-assignments/LD4.1/a.out",
+			     "a.out",
+			     msg,
+			     NULL
+			);
 		}
 
 	} else {
