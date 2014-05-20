b44472974d388f172877d1cee9bfce3c182d40db
martass <martynas12@gmail.com>
2014-05-20 16:25:21 +0300

    ld41/exec/unfinised

--- LD4.1/loginas_exec01.c
+++ LD4.1/loginas_exec01.c
@@ -5,8 +5,25 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[])
 {
+	if(argc == 2) {
+		int arg = atoi(argv[1]);
+		pid_t pid = getpid();
+      	 	pid_t ppid = getppid();
+		printf("PID: %ld\t PPID: %ld\t ARG: %d\n", pid, ppid, arg);
+			
+		if(arg > 0)
+		{
+			--arg;
+			char *const params[] = {(char*)arg};
+			execv(".", params);
+		}
 
+	} else {
+		puts("invalid number of args. expected only 1.");
+	}	
+	return 0;
 }
